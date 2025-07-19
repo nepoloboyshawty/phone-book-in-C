01b72d2e1789430e3768ef014646f7aed9810752
@nepbs <sergeyastakhov24@gmail.com>
2025-07-19 12:52:23 +0300

    Update contacts.h

--- includes/contacts.h
+++ includes/contacts.h
@@ -1,1 +1,12 @@
+#ifndef __CONTACTS_H__
+#define __CONTACTS_H__
 
+#include <stdio.h>
+
+void record_contact(person_info*);
+int сompare_data(const void*, const void*);
+void print_contacts(void);
+void show_contacts(void);
+void delete_contact(void);
+
+#endif
